a606d34fd1f67b139981c1248d22e88753585269
Emlyn Price <emlyn.je.price@gmail.com>
2017-02-19 16:14:51 +0000

    Look in ssh config for key locations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,5 @@ alias e="emc"
 alias enw="emacsclient -t"
 
 if status --is-interactive
-        keychain --eval --nogui --quiet -Q id_rsa homebitbucket github | source
+        keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket github > /dev/null
 end
