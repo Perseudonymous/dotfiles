a9b1007b2fc03fc8a56adf264066ba181ddf5150
Emlyn Price <emlyn.je.price@gmail.com>
2017-03-17 13:47:22 +0000

    Define a function to add keys to keychain.
    It was still not adding the keys it should.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,10 +9,16 @@ if test (hostname) = "tinker"
     set -x THIS_MACHINE tinker
 end
 
+function add_tinker_keys
+    keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
+    keychain --eval --nogui --quiet -Q --confhost github > /dev/null
+end
+
 # Only do keychain stuff if on tinker
 if test $THIS_MACHINE = "tinker":
     if status --is-interactive
-        keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
-        keychain --eval --nogui --quiet -Q --confhost github > /dev/null
+        #keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
+        #keychain --eval --nogui --quiet -Q --confhost github > /dev/null
+        add_tinker_keys
     end
 end
