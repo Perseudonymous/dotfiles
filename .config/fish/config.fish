6b10b50d47d2a9749fbf7016ea681a60a66f7508
Emlyn Price <emlyn.je.price@gmail.com>
2017-03-20 19:39:22 +0000

    Remove keychain code from config.fish
    I think something to do with not having fish set as my default shell was causing
    problems with keychain. Add the keys in .bashrc for now.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,13 +7,16 @@ alias enw="emacsclient -t"
 # Check hostname
 if test (hostname) = "tinker"
     set -x THIS_MACHINE tinker
+    #echo "Set tinker"
 end
 
 # Only do keychain stuff if on tinker
-if test $THIS_MACHINE = "tinker":
-    #if status --is-interactive
-        #keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
-        #keychain --eval --nogui --quiet -Q --confhost github > /dev/null
-    add_tinker_keys
-    #end
-end
+# if test $THIS_MACHINE = "tinker"
+#     echo "Is tinker"
+#     if status --is-interactive
+#         #keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
+#         keychain --eval --nogui --quiet -Q --confhost github | source
+#     #add_tinker_keys
+#     echo "Added keys"
+#     end
+# end
--- .config/fish/functions/add_tinker_keys.fish
+++ .config/fish/functions/add_tinker_keys.fish
@@ -1,6 +1,7 @@
 function add_tinker_keys
     if status --is-interactive
-        keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
-        keychain --eval --nogui --quiet -Q --confhost github > /dev/null
+        #keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
+        echo "Is interactive"
+        keychain --eval --nogui --quiet -Q --confhost github 
     end
 end
