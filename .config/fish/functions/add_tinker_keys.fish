c19c386512136311b628871b07f8353fb2e9a09b
Emlyn Price <emlyn.je.price@gmail.com>
2017-03-20 18:59:04 +0000

    uhhhh

--- .config/fish/functions/add_tinker_keys.fish
+++ .config/fish/functions/add_tinker_keys.fish
@@ -1,4 +1,4 @@
 function add_tinker_keys
-    keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
-    keychain --eval --nogui --quiet -Q --confhost github > /dev/null
+    #keychain --eval --nogui --quiet -Q --confhost id_rsa homebitbucket > /dev/null
+    keychain --eval --nogui --quiet -Q --confhost github
 end
